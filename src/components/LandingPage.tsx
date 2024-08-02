0272bcba860d4d5b541ea8a2c2ed23dd7f6435d4
Lateef Mushtaq <lateef9thncs@gmail.com>
2024-08-02 17:33:17 +0100

    Refine homepage landing section layout and design

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -3,10 +3,16 @@ import mainImage from "../assets/images/products/sonyXb910n-1.png";
 function LandingPage() {
   return (
     <div className="main">
-      <div className="details">SONY</div>
+      <div className="image-info">
+        <h4>Sony H4-XB72</h4>
+        <h1>Give your favourite music a boost</h1>
+        <h4>
+          $1200 <span id="discount">$2000</span>
+        </h4>
+        <button id="ShopButton">Shop Now</button>
+      </div>
       <div className="image-container">
-        {" "}
-        <img src={mainImage}></img>
+        <img id="image-1" src={mainImage}></img>
       </div>
     </div>
   );
