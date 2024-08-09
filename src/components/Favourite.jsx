e48d0f1bc615549b65569bb9a418d05e3bcd26de
Lateef Mushtaq <lateef9thncs@gmail.com>
2024-08-09 12:15:37 +0100

    Add 'Add to Cart' functionality to items in the Favorites list
    Users can now directly add items to their shopping cart from their favorites list.
    This improves the user experience by streamlining the shopping process.

--- src/components/Favourite.jsx
+++ src/components/Favourite.jsx
@@ -1,42 +1,59 @@
-import React, { useState, useContext, useMemo } from "react";
+import React, { useContext } from "react";
 import { ProductContext } from "../context/ProductsProvider";
 import { MdDeleteOutline } from "react-icons/md";
+import { handleClick } from "../utils/helper";
+import { BiCartAdd } from "react-icons/bi";
+import "../styles/Favourite.css";
+
 export default function Favourite() {
-  const { setFavourite, favourite } = useContext(ProductContext);
+  const {
+    favourite,
+    setFavourite,
+    setCartData,
+    setNotify,
+    setMessage,
+    setType,
+    setCount,
+    Products,
+  } = useContext(ProductContext);
+
   function handleRemove(id) {
     setFavourite(favourite.filter((item) => item.id !== id));
   }
+
   return (
-    <>
-      <div style={{ height: "75vh" }}>
-        {favourite.map((item, i) => (
-          <div
-            className="main"
-            key={item.id}
-            style={{ backgroundColor: "#2d2d2d" }}
-          >
-            <p>{i + 1}</p>
-            <div
-              className="image-container-fav"
-              style={{
-                width: "50px",
-              }}
-            >
-              <img
-                src={item.images[0]}
-                alt={item.title}
-                style={{ width: "100%" }}
-              />
-            </div>
-            <p style={{ color: "green" }}> {item.title}</p>{" "}
+    <div style={{ height: "75vh" }}>
+      {favourite.map((item, i) => (
+        <div className="main" key={item.id}>
+          <p>{i + 1}</p>
+          <div className="image-container-fav">
+            <img src={item.images[0]} alt={item.title} />
+          </div>
+          <p className="item-title">{item.title}</p>
+          <div style={{ display: "flex", gap: "24px" }}>
             <MdDeleteOutline
-              size={"32px"}
-              color="red"
+              size="32px"
+              className="delete-icon"
               onClick={() => handleRemove(item.id)}
             />
+
+            <BiCartAdd
+              className="cart-button-icon"
+              onClick={() =>
+                handleClick(
+                  item.id,
+                  setCartData,
+                  setNotify,
+                  setMessage,
+                  setType,
+                  setCount,
+                  Products
+                )
+              }
+            />
           </div>
-        ))}
-      </div>
-    </>
+        </div>
+      ))}
+    </div>
   );
 }
