cff2c2daac9ded05c97f1e439a1ba2e3a858210b
Lateef Mushtaq <lateef9thncs@gmail.com>
2024-09-19 16:06:43 +0200

    Made changes in Nav and implemented Nav bar from MUI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,62 +1,251 @@
 import { Link, useNavigate } from "react-router-dom";
+import { Login } from "./Login";
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import Badge from "@mui/material/Badge";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import AccountCircle from "@mui/icons-material/AccountCircle";
 import { FaCartArrowDown } from "react-icons/fa6";
+import MoreIcon from "@mui/icons-material/MoreVert";
 import { ProductContext } from "../context/ProductsProvider";
 import { useContext, useState } from "react";
-import { FaRegHeart } from "react-icons/fa";
-import { LuUser2 } from "react-icons/lu";
-import { Login } from "./Login";
-import { CiSearch } from "react-icons/ci";
-import "../styles/Navbar.css";
-function Navbar() {
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
+
+export default function Navbar() {
   const { cartData, favourite, isModalOpen, setModalOpen } =
     useContext(ProductContext);
   const openModal = () => setModalOpen(true);
   const closeModal = () => setModalOpen(false);
   const [search, setSearch] = useState("");
   const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
   function handleSearch() {
     search.length > 0 && navigate(`/search?q=${search}`);
   }
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = "primary-search-account-menu";
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = "primary-search-account-menu-mobile";
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={favourite && favourite.length} color="error">
+            <FavoriteBorderIcon />
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton
+          size="large"
+          aria-label="show 17 new notifications"
+          color="inherit"
+        >
+          <Badge badgeContent={cartData && cartData.length} color="error">
+            <FavoriteBorderIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
 
   return (
-    <header className="header">
-      <div className="logo">
-        <Link to={"/"}>eShop</Link>
-      </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed" sx={{ backgroundColor: "#000000" }}>
+        <Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ display: { xs: "none", sm: "block", cursor: "pointer" } }}
+            onClick={() => navigate("/home")}
+          >
+            eShop
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              onChange={(e) => setSearch(e.target.value)}
+              inputProps={{ "aria-label": "search" }}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") handleSearch();
+              }}
+            />
+          </Search>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <Link to={"/favourite"}>
+              <IconButton
+                size="medium"
+                aria-label="show 4 new mails"
+                sx={{ color: "#fff6f6" }}
+              >
+                <Badge badgeContent={favourite.length} color="error">
+                  <FavoriteBorderIcon />
+                </Badge>
+              </IconButton>
+            </Link>
 
-      <div className="navIcons">
-        <div className="search">
-          <input
-            id="search"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <CiSearch id="searchIcon" size={"24px"} onClick={handleSearch} />
-        </div>
-        <div>
-          {" "}
-          <LuUser2 size={"24px"} onClick={openModal} />
-          <span className="badge badge-warning" id="lblCartCount">
-            {"!"}
-          </span>
-        </div>
-        <Login isOpen={isModalOpen} onClose={closeModal} />
-        <Link to={"/favourite"}>
-          <FaRegHeart size={"24px"} />
-          <span className="badge badge-warning" id="lblCartCount">
-            {favourite && favourite.length}
-          </span>
-        </Link>
-        <Link to={"/cart"}>
-          <FaCartArrowDown size={"24px"} />
-          <span className="badge badge-warning" id="lblCartCount">
-            {cartData && cartData.length}
-          </span>
-        </Link>
-      </div>
-    </header>
+            <Link to={"/cart"}>
+              <IconButton
+                size="medium"
+                aria-label="show 17 new notifications"
+                sx={{ color: "#fff6f6" }}
+              >
+                <Badge badgeContent={cartData && cartData.length} color="error">
+                  <FaCartArrowDown />
+                </Badge>
+              </IconButton>
+            </Link>
+
+            <IconButton
+              size="medium"
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={openModal}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </Box>
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+      <Login isOpen={isModalOpen} onClose={closeModal} />
+    </Box>
   );
 }
-
-export default Navbar;
